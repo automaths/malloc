2e4301471948c4189483c09bcc5e9afcaa9dfa2b
automaths <nicolassartral@gmail.com>
2024-01-28 19:08:20 +0100

    more precise print, debug seg

--- src/malloc.c
+++ src/malloc.c
@@ -1,4 +1,5 @@
 #include "ft_malloc.h"
+#include <stdio.h>
 
 extern t_mem ft_data;
 
@@ -60,100 +61,69 @@ static size_t which_alloc_size(size_t size) {
 static void* create_new_mem_segment(size_t size)
 {
     t_mem *mem_ptr = &ft_data;
-    void *ret_ptr = NULL;
     size_t i = 0;
     size_t mem_size = which_mem_size(size);
     size_t alloc_size = which_alloc_size(size);
-    (void)alloc_size;
-
-    void* ptr;
-    ptr = mmap(NULL, mem_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (mem_ptr->next == MAP_FAILED)
-        return (write(1, "Error: mmap failed\n", 19), NULL);
 
     if (mem_ptr->next == NULL) {
-        mem_ptr = (t_mem*)ptr;
+        if (mem_ptr->next == MAP_FAILED)
         mem_ptr->prev = NULL;
     } else {
         while (mem_ptr->next != NULL) 
             mem_ptr = mem_ptr->next;
-        mem_ptr->next = (t_mem*)ptr;
+        if (mem_ptr->next == MAP_FAILED)
         mem_ptr->next->prev = mem_ptr;
         mem_ptr = mem_ptr->next;
     }
+
     mem_ptr->size = mem_size;
     mem_ptr->next = NULL;
     i += MEM_METADATA_SIZE;
 
-    mem_ptr->first_alloc = (t_alloc*)((char*)(ptr + i));
+    mem_ptr->first_alloc = (t_alloc*)((char*)(mem_ptr + i));
     i += ALLOC_METADATA_SIZE;
     t_alloc* alloc_ptr = mem_ptr->first_alloc;
     alloc_ptr->prev = NULL;
     alloc_ptr->next = NULL;
     alloc_ptr->is_free = true;
     alloc_ptr->size = alloc_size;
-    alloc_ptr->ptr = (void*)((char*)(ptr + i));
-    i += TINY_ALLOC_SPACE;
-    ret_ptr = alloc_ptr->ptr;
+    alloc_ptr->ptr = (void*)((char*)(mem_ptr + i));
+    i += alloc_size - ALLOC_METADATA_SIZE;
+    void* ret_ptr = alloc_ptr->ptr;
 
-    size_t j = 1;
-    while (i < mem_size) {
-        alloc_ptr->next = (t_alloc*)((char*)ptr + i);
+    while ((i + alloc_size) < mem_size) {
+        printVoidPointerAddressInHex((void*)(mem_ptr + i), "loop ");
+        alloc_ptr->next = (t_alloc*)((char*)(mem_ptr + i));
         i += ALLOC_METADATA_SIZE;
+        printVoidPointerAddressInHex((void*)(mem_ptr + i), "loop ");
         alloc_ptr->next->prev = alloc_ptr;
         alloc_ptr = alloc_ptr->next;
+        printVoidPointerAddressInHex((void*)(mem_ptr + i), "loop ");
         alloc_ptr->is_free = true;
-        alloc_ptr->ptr = ((char*)ptr + i);
+        alloc_ptr->ptr = ((char*)(mem_ptr + i));
         alloc_ptr->size = alloc_size;
         alloc_ptr->next = NULL;
-        i += TINY_ALLOC_SPACE;
-        ++j;
+        i += alloc_size - ALLOC_METADATA_SIZE;
+        printVoidPointerAddressInHex((void*)(mem_ptr + i), "loop ");
     }
-    write(1, "i: ", 3);
-    ft_putnbr_fd(i, 1);
-    write(1, "\n", 1);
-    write(1, "mem_size: ", 10);
-    ft_putnbr_fd(mem_size, 1);
-    write(1, "\n", 1);
-    write(1, "j: ", 3);
-    ft_putnbr_fd(j, 1);
-    write(1, "\n", 1);
-
-    // while (++i < 126) // if 128, corruption warning at exec 
-    // {
-    //     j += alloc_size;
-    //     alloc_ptr->next = (t_alloc*)((char*)alloc_ptr + alloc_size);
-    //     alloc_ptr->next->prev = alloc_ptr;
-    //     alloc_ptr = alloc_ptr->next;
-    //     alloc_ptr->ptr = ((char*)alloc_ptr + ALLOC_METADATA_SIZE);
-    //     alloc_ptr->is_free = true;
-    //     alloc_ptr->next = NULL;
-    // }
-    // while (alloc_ptr->prev != NULL){
-    //     alloc_ptr = alloc_ptr->prev;
-    // }
-    // while (mem_ptr->prev != NULL){
-    //     mem_ptr = mem_ptr->prev;
-    // }
+    while (alloc_ptr->prev != NULL){
+        alloc_ptr = alloc_ptr->prev;
+    }
+    while (mem_ptr->prev != NULL){
+        mem_ptr = mem_ptr->prev;
+    }
     return (ret_ptr);
 }
 
 void *malloc(size_t size)
 {
     void *ptr = NULL;
-    // unsigned char* ptr = (unsigned char*)mmap(NULL, 10, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    // ptr = (unsigned char*)ptr;
-    // ptr[0] = 'c';
-    // (void)ptr;
-    // write (1, ptr, 1);
 
     // ptr = search_available_mem_segment(size);
-    // if (0) {
-        if (ptr == NULL){
-            ptr = create_new_mem_segment(size);
-        }
-        show_alloc_mem();
-    // }
+    if (ptr == NULL){
+        ptr = create_new_mem_segment(size);
+    }
+    show_alloc_mem();
 
     return (NULL);
 }
--- src/utils.c
+++ src/utils.c
@@ -3,18 +3,22 @@
 
 extern t_mem ft_data;
 
-void printVoidPointerAddressInHex(void *ptr) {
-    uintptr_t address = (uintptr_t)ptr;
-    char addressString[20];
-    char *addressPtr = addressString;
+void printVoidPointerAddressInHex(void *ptr, char *debug) {
+    // uintptr_t address = (uintptr_t)ptr;
+    // char addressString[20];
+    // char *addressPtr = addressString;
 
-    *addressPtr++ = '0';
-    *addressPtr++ = 'x';
-    for (size_t i = 0; i < 5; ++i) {
-        *addressPtr++ = "0123456789abcdef"[(address >> ((sizeof(uintptr_t) * 2 - 1 - i) * 4)) & 0xF];
-    }
-    *addressPtr = '\0';
-    write(1, addressString, 7);
+    // *addressPtr++ = '0';
+    // *addressPtr++ = 'x';
+    // for (size_t i = 0; i < 5; ++i) {
+    //     *addressPtr++ = "0123456789abcdef"[(address >> ((sizeof(uintptr_t) * 2 - 1 - i) * 4)) & 0xF];
+    // }
+    // *addressPtr = '\0';
+    // write(1, addressString, 7);
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), "%p\n", ptr);
+    write(1, debug, ft_strlen(debug));
+    write(1, buffer, ft_strlen(buffer));
 }
 
 void print_allocation_strat(void) {
@@ -41,43 +45,37 @@ void show_alloc_mem(void) {
 
     t_mem *mem_ptr = &ft_data;
 
-    write(1, "\n\n", 2);
-    while (mem_ptr->next != NULL)
+    while (mem_ptr != NULL)
     {
-        if ((int)mem_ptr->size <= TINY_ZONE_SIZE)
-            write(1, "TINY : ", 7);
-        else if ((int)mem_ptr->size <= SMALL_ZONE_SIZE)
-            write(1, "SMALL : ", 8);
-        else
-            write(1, "LARGE : ", 8);
-        printVoidPointerAddressInHex((void*)mem_ptr);
+        // if ((int)mem_ptr->size <= TINY_ZONE_SIZE)
+        //     write(1, "TINY : ", 7);
+        // else if ((int)mem_ptr->size <= SMALL_ZONE_SIZE)
+        //     write(1, "SMALL : ", 8);
+        // else
+        //     write(1, "LARGE : ", 8);
 
         t_alloc *alloc_ptr = mem_ptr->first_alloc;
-        (void)alloc_ptr;
-        // write(1, "coucou\n", 7);
-        if (alloc_ptr->prev == NULL)
-            write(1, "coucoa\n", 7);
-        if (alloc_ptr->next == NULL)
-            write(1, "coucou\n", 7);
         while (alloc_ptr->next != NULL)
         {
-            write(1, "coucou\n", 7);
-        //     if (alloc_ptr->is_free == 0)
-        //     {
-        //         write(1, "\n", 1);
-        //         printVoidPointerAddressInHex((void*)alloc_ptr->ptr);
-        //         write(1, " - ", 3);
-        //         printVoidPointerAddressInHex((void*)alloc_ptr->ptr + alloc_ptr->size);
-        //         write(1, " : ", 3);
-        //         ft_putnbr_fd(alloc_ptr->size, 1);
-        //         write(1, " bytes", 7);
-        //     }
-            write(1, "coucou\n", 7);
-            // alloc_ptr = alloc_ptr->next;
+            if (alloc_ptr->is_free == true)
+            {
+                write(1, "\n", 1);
+                printVoidPointerAddressInHex((void*)alloc_ptr->ptr, "");
+                write(1, " - ", 3);
+                printVoidPointerAddressInHex((void*)alloc_ptr->ptr + alloc_ptr->size, "");
+                write(1, " : ", 3);
+                ft_putnbr_fd(alloc_ptr->size, 1);
+                write(1, " bytes", 7);
+            }
+            alloc_ptr = alloc_ptr->next;
         }
-
+        while (alloc_ptr->prev != NULL)
+            alloc_ptr = alloc_ptr->prev;
+        if (mem_ptr->next == NULL)
+            break;
         mem_ptr = mem_ptr->next;
     }
-    write(1, "\n\n", 2);
+    while (mem_ptr->prev != NULL)
+        mem_ptr = mem_ptr->prev;
 }
 
