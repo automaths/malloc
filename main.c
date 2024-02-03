b666bfdc8e1101650ebbd81a36e11f7446231a1e
automaths <nicolassartral@gmail.com>
2024-02-03 16:48:53 +0100

    realloc fix

--- main.c
+++ main.c
@@ -4,20 +4,22 @@ t_mem* ft_data;
 
 int main()
 {
-    // void* ptr = malloc(0);
-    // void* ptr2 = malloc(50);
-    // ft_putnbr_fd(maxStackSize(), 1);    
-    // write(1, "coucou\n", 7);
+    void* ptr = malloc(0);
+    void* ptr2 = malloc(50);
     void* ptr3 = malloc(5000000000000000000);
     if (ptr3 == NULL)
         write(1, "NULL\n", 5);
-    // write(1, "coucou\n", 7);
-    // (void)ptr2;
+    void* ptr4 = malloc(500);
+    void* ptr5 = malloc(5000);
+    (void)ptr2;
     // (void)ptr3;
+    write(1, "----------------\n", 17);
     show_alloc_mem();
-    // free(ptr);
-    // ptr = realloc(ptr, 500);
-    // show_alloc_mem();
+    free(ptr);
+    void* ptr6 = realloc(NULL, 500);
+    free(ptr3);
+    write(1, "----------------\n", 17);
+    show_alloc_mem();
     // printAllocationStrat();
 
     return (0);
--- src/realloc.c
+++ src/realloc.c
@@ -6,6 +6,9 @@ void *realloc(void *ptr, size_t size)
 {
     void *ret_ptr = malloc(size);
 
+    size_t max_stack_size = maxStackSize();
+    if (size > max_stack_size)
+        size = max_stack_size;
     if (ptr != NULL) {
         t_alloc* ptr_metadata = find_ptr_meta(ptr);
         size_t cp_size = ptr_metadata->size < size ? ptr_metadata->size : size;
